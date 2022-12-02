b132ea7c20f99b26ae95e85e2a2ec1a6c4704063
whiplash0104 <felipe.basso.vera@gmail.com>
2022-12-02 18:17:54 -0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo runuser -l opc -c "kubectl create namespace hello-python"
+sudo runuser -l opc -c "kubectl create secret docker-registry ocirsecret --docker-server=${REGION}/${REGISTRY_NAMESPACE} --docker-username=${USER} --docker-password='${TOKEN}' -n hello-python"
+sudo runuser -l opc -c "kubectl apply -f deployment.yml"
