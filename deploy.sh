589b3a176ade87c5dcce474627a60a37174bdedf
whiplash0104 <felipe.basso.vera@gmail.com>
2022-12-02 18:45:27 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 sudo runuser -l opc -c "kubectl create namespace hello-python"
 sudo runuser -l opc -c "kubectl create secret docker-registry ocirsecret --docker-server=${REGION}/${REGISTRY_NAMESPACE} --docker-username=${USER} --docker-password='${TOKEN}' -n hello-python"
-sudo runuser -l opc -c "kubectl apply -f deployment.yaml"
+sudo runuser -l opc -c "kubectl apply -f /var/lib/jenkins/workspace/hello-python-OKE_master/deployment.yaml"
