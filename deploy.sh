f32876e1ec12664ab6c498367fc2166db9e94d0d
whiplash0104 <felipe.basso.vera@gmail.com>
2022-12-12 15:43:58 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+DIR=$(pwd)
 
-sudo runuser -l opc -c "kubectl create namespace ${OCINAMESPACE}"
-sudo runuser -l opc -c "kubectl create secret docker-registry ocirsecret --docker-server=${REGION}/${REGISTRY_NAMESPACE} --docker-username=${OCIUSER} --docker-password='${TOKEN}' -n ${OCINAMESPACE}"
-sudo runuser -l opc -c "kubectl apply -f deployment.yaml"
+if [ -z "$(kubectl get namespace | grep hello-oke)" ]; then 
+  kubectl create namespace ${OCINAMESPACE};
+  kubectl create secret docker-registry ocirsecret --docker-server=${REGION}/${REGISTRY_NAMESPACE} --docker-username=${OCIUSER} --docker-password="${TOKEN}" -n ${OCINAMESPACE};
+fi
+
+sudo runuser -l opc -c "kubectl apply -f ${DIR}/deployment.yaml
